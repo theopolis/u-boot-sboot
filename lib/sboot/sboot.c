a5c2bd6cc9a7ef8e288f68ace147950dfcb7a146
Teddy <teddy@prosauce.org>
2012-12-20 13:29:07 -0700

    Short change to sboot.c, example use of TlclSeal implementation.

--- lib/sboot/sboot.c
+++ lib/sboot/sboot.c
@@ -5,7 +5,37 @@
 
 static uint8_t sboot_seal(void)
 {
-	/* Architecture question, should this be implemented in vboot's TPM lib? */
+	uint32_t result;
+
+	uint32_t keyHandle;
+	uint32_t pcrMap;
+	uint8_t keyAuth[20];
+	uint8_t dataAuth[20];
+
+	uint8_t data[20];
+	uint8_t blob[256];
+	uint32_t blobSize;
+
+	keyHandle = 0x40000000; /*SRK*/
+	pcrMap = 0 + (1 << SBOOT_PCR_UBOOT) + (1 << SBOOT_PCR_KERNEL);
+	/* todo: keyAuth = SHA1("password1")
+	 * todo: dataAuth = SHA1("password2")
+	 */
+	memset(keyAuth, 0, 20);
+	memset(dataAuth, 0, 20);
+
+	/* todo: data = SHA1(some input param) */
+
+	result = TlclSealPCR(keyHandle, pcrMap, keyAuth, dataAuth,
+		data, 20 /*TPM_HASH_SIZE*/, blob, &blobSize);
+
+	if (result != 0) {
+		/* problem */
+		return -1;
+	}
+
+	/* todo: write blob to NV */
+
 	return 0;
 }
 
